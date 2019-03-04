87af4a8786e35ce69806c1f710c70b7db92ae114
admin <admin>
2019-03-04 19:11:27 +0800

    Jay 1.新建 心跳机制 类

---
+++ app/src/main/java/com/jay/cloud_board/HeartBeat.java
@@ -0,0 +1,13 @@
+package com.jay.cloud_board;
+
+/**
+ * @Description 心跳包机制
+ * Created by jj on 2019/3/4.
+ */
+
+public class HeartBeat implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
