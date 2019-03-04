1e42c01272929195f9e0e346fe4c9c947f75ea03
admin <admin>
2019-03-04 19:14:20 +0800

    Jay 1.修改 心跳机制类:HearBeat-->HeartBeatTask

--- app/src/main/java/com/jay/cloud_board/HeartBeatTask.java
+++ app/src/main/java/com/jay/cloud_board/HeartBeatTask.java
@@ -5,7 +5,7 @@ package com.jay.cloud_board;
  * Created by jj on 2019/3/4.
  */
 
-public class HeartBeat implements Runnable {
+public class HeartBeatTask implements Runnable {
     @Override
     public void run() {
 
